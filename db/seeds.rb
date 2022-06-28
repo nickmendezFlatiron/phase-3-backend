4badd92a7fcb9b6da57a7d258bb10bbb992155cc
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-06-28 17:09:21 -0400

    appended seed data using faker to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+
+# create dogs
+20.times do
+  Dog.create({
+    dog_name: Faker::Creature::Dog.name ,
+    dog_weight: Faker::Number.number(digits: 2) ,
+    dog_image: nil ,
+    owner_id: nil
+  })
+end 
+
+
 puts "✅ Done seeding!"
